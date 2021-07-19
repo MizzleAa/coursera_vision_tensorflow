cec36dd103fe4682680a2475d50db7fe0c052f6a
MizzleAa <mizzleaaa@gmail.com>
2021-07-19 13:25:21 +0900

    :memo: lunch time study

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+import re
+import time
+import json
+import PIL.Image
+import PIL.ImageFont
+import PIL.ImageDraw
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+from tensorflow.keras.applications.resnet50 import ResNet50
+from matplotlib import pyplot as plt
+
+print("Tensorflow version " + tf.__version__)
